c75fd39bf4e505ee06485159ab2b39d2b5fd83ea
TANguyen1893 <ta.nguyen1893@gmail.com>
2016-02-20 02:02:03 -0500

    Implemented first test for invalid params

--- app/src/androidTest/java/com/example/bernie/postalcalculator/CalculatorTest.java
+++ app/src/androidTest/java/com/example/bernie/postalcalculator/CalculatorTest.java
@@ -10,6 +10,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
+import junit.framework.Assert;
+
 /**
  * Created by Bernie on 19/02/2016.
  */
@@ -40,19 +42,36 @@ public class CalculatorTest extends ActivityInstrumentationTestCase2<MainActivit
         weight = (EditText) activity.findViewById(R.id.editText);
     }
 
-    public void tearDown() throws Exception
-    {
-        activity.finish();
-    }
 
+
+    // This tests invalid inputs for weight. Country, category, and item type have a default value by design
+    // so it would be impossible to set it to any other value than the ones present in the drop down
     @UiThreadTest
-    public void testNotEnoughParams()
+    public void testInvalidParams()
     {
         // This is how you set the value of a spinner
         ArrayAdapter adapter = (ArrayAdapter) country.getAdapter();
         int position = adapter.getPosition("Canada"); // US or International
         country.setSelection(position);
 
+        ArrayAdapter adapter2 = (ArrayAdapter) category.getAdapter();
+        int position2 = adapter2.getPosition("Standard Letter-Post"); // US or International
+        category.setSelection(position2);
+
+        ArrayAdapter adapter3 = (ArrayAdapter) itemType.getAdapter();
+        int position3 = adapter3.getPosition("Stamps in booklets/coils/panes"); // US or International
+        itemType.setSelection(position3);
+
+        weight.setText("-1"); // Set weight to a negative number. Also possible to set it to letters
+
+        calculateButton.performClick();
+
+        assertEquals(-1.0, Calculator.calcStandardInCanada(ItemType.StampsInBookletsCoilsPanes, Double.valueOf(weight.getText().toString())));
+    }
+
+        public void tearDown() throws Exception
+    {
+        activity.finish();
     }
 
 

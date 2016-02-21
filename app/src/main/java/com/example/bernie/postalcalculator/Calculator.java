3263d5b50a1ccbdb54fecf257a82134d74106944
Bernard <Bernard.kaminski@mail.mcgill.ca>
2016-02-21 01:08:42 -0500

    fixed bug

--- app/src/main/java/com/example/bernie/postalcalculator/Calculator.java
+++ app/src/main/java/com/example/bernie/postalcalculator/Calculator.java
@@ -57,7 +57,7 @@ public class Calculator {
                 if(weight >300)
                     return 4.42;
                 if(weight >200)
-                    return 4.89;
+                    return 3.89;
                 if(weight >100)
                     return 2.77;
                 if(weight >0)
